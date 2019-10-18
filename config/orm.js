3d81de9386d1ed347f0760e6626a1a2d0a40d2e2
Nicholas Ready <Ready.nicholas@gmail.com>
2019-10-18 15:34:40 -0500

    with heroku settings

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
